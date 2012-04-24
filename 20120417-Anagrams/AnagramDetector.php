2a736f962ab3a3873554147cbabee47c2653fc6d
Eric Hogue <eric@erichogue.ca>
2012-04-24 08:58:53 -0400

    Detected anagram in an array

--- 20120417-Anagrams/AnagramDetector.php
+++ 20120417-Anagrams/AnagramDetector.php
@@ -2,6 +2,28 @@
 
 class AnagramDetector {
 	public function getAnagrams(Array $words) {
-		return array();
+		$wordsByKey = $this->getWordsArray($words);
+		return array_filter($wordsByKey, function($value) {
+			return count($value) > 1;
+		});
 	}
+
+	protected function getWordsArray($originalWords) {
+		$wordsByKey = array();
+
+		foreach($originalWords as $plainWord) {
+			$wordObject = new Word($plainWord);
+			$key = $wordObject->getKey();
+
+			if (!array_key_exists($key, $wordsByKey)) {
+				$wordsByKey[$key] = array();
+			}
+
+			$wordsByKey[$key][] = $wordObject;
+		}
+
+		return $wordsByKey;
+	}
+
+
 }
--- 20120417-Anagrams/Tests/AnagramDetectorTest.php
+++ 20120417-Anagrams/Tests/AnagramDetectorTest.php
@@ -1,17 +1,26 @@
 <?php
 
 class AnagramDetectorTest extends PHPUnit_Framework_TestCase {
+	/** @var AnagramDetector */
+	private $detector;
 
-	public function testCreate() {
-		$this->assertNotNull(new AnagramDetector());
+	public function setup() {
+		$this->detector = new AnagramDetector();
 	}
 
 	public function testEmptyListHasNoAnagram() {
-		$detector = new AnagramDetector();
-		$this->assertEmpty($detector->getAnagrams(array()));
+		$this->assertEmpty($this->detector->getAnagrams(array()));
 	}
 
 	public function testOneWordAsNoAnagram() {
+		$this->assertEmpty($this->detector->getAnagrams(array('eric')));
+	}
 
+	public function testTwoWordsAreNotAnagram() {
+		$this->assertEmpty($this->detector->getAnagrams(array('eric', 'jfdslk')));
+	}
+
+	public function testTwoAnagramsAreDetected() {
+		$this->assertCount(1, $this->detector->getAnagrams(array('eric', 'ci-re')));
 	}
 }
